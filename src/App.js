d66312888f11c16692c5f7f1124b62ac2a476402
Shivani <c.vaani.dauneria@gmail.com>
2023-04-02 17:46:06 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import CartItems from './Components/CartItems/CartItems';
+import Home from './Components/Home/Home';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
+  const [showCart, setShowCart] = useState(false)
+  const [addProduct, setProduct] = useState([])
+  const [reRender, setReRender] = useState(false)
+  const handleRender = () => {
+    setReRender(!reRender)
+  }
+
+  const addToCart = (brand, image, price, id, index) => {
+   
+    if(addProduct.some(product => product.id === id)){
+      addProduct.splice(index, 1)
+    }
+    
+    else{
+      setProduct([
+        ...addProduct,
+        {
+          productName: brand,
+          price: price,
+          image: image,
+          id: id,
+          qty: 1
+        }
+      ]);
+    }
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <NavBar setShowCart={setShowCart} showCart={showCart} productCounter={addProduct.length} handleRender={handleRender} reRender={reRender}/>
+      <CartItems showCart={showCart} addProduct={addProduct}  handleRender={handleRender} reRender={reRender}/>
+      <Home addToCart={addToCart}/>
     </div>
   );
 }
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+import './NavBar.css'
+import { FiShoppingCart } from 'react-icons/fi';
+
+const NavBar = ({setShowCart, showCart, productCounter, reRender}) => {
+    useEffect(()=>{
+
+    }, [reRender])
+    return (
+        <div className='navBarContainer'>
+            <div className="navInner">
+                <div className="logo">
+                    <img src="https://www.freepnglogos.com/uploads/shopping-cart-png/shopping-cart-campus-recreation-university-nebraska-lincoln-30.png" alt="" />
+                </div>
+                <div>
+                    <h1 className='cart'>ShoppingCart</h1>
+                </div>
+                <div className="cartIcon" onClick={()=>setShowCart(!showCart)}><FiShoppingCart size={25} />
+                    <div className="productCount">{productCounter}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
